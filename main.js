d6ad62f7b87a92ed85c376c0ef97b66867f717a9
Milikrili <57447670+Milikrili@users.noreply.github.com>
2020-09-20 22:16:23 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var strelicaLevo= document.getElementById('levo');
 var strelicaDesno= document.getElementById('desno');
 var slika= document.getElementById('karuselSlika');
 
-var nizSlika=[ 'img/st_prevoz_01.jpg','img/st_prevoz_02.jpg','img/st_prevoz_03.jpg','img/st_prevoz_04.jpg','img/st_prevoz_05.jpg','img/st_prevoz_06.jpg','img/st_prevoz_07.jpg','img/st_prevoz_08.jpg','img/st_prevoz_09.jpg','img/st_prevoz_10.jpg','img/st_prevoz_11.jpg','img/st_prevoz_12.jpg','img/st_prevoz_13.jpg'];
+var nizSlika=[ 'img/st_prevoz_01.jpg','img/st_prevoz_03.jpg','img/st_prevoz_04.jpg','img/st_prevoz_05.jpg','img/st_prevoz_06.jpg','img/st_prevoz_07.jpg','img/st_prevoz_08.jpg','img/st_prevoz_12.jpg','img/st_prevoz_13.jpg'];
 var i=0;
 
 console.log(strelicaLevo);
@@ -31,24 +31,3 @@ strelicaDesno.addEventListener('click', naredna);
 setInterval(naredna, 3000);
 
 
-var burgerPrikazi = document.getElementById('burger');
-var navNavigacija = document.getElementById('navigacija');
-
-
-function toggle() {
-    console.log(navNavigacija.style.display);
-
-
-
-    if (navNavigacija.style.display == "none") {
-        navNavigacija.style.display = "block";
-    }
-    else {
-        navNavigacija.style.display = "none";
-    }
-
-
-}
-
-
-burgerPrikazi.addEventListener('click', toggle);
